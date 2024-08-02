72fad22103ed30cf9ee36c30a5cc157742cbde87
shubh <shubh@Shubham>
2024-08-02 23:11:02 +0530

    Added fibonacci class

---
+++ javaPracticePrograms/src/javaPracticePrograms/Fibonacci.java
@@ -0,0 +1,15 @@
+package javaPracticePrograms;
+
+public class Fibonacci {
+	
+	
+	public static void generateFibonacci() {
+		//write method here
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
