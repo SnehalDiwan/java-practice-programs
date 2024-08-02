1db4a9ee6d2f3e670f4572ef03d29ab42ccbc150
shubh <shubh@Shubham>
2024-08-02 23:09:14 +0530

    added one more syso

--- javaPracticePrograms/src/javaPracticePrograms/PracticeA.java
+++ javaPracticePrograms/src/javaPracticePrograms/PracticeA.java
@@ -5,6 +5,7 @@ public class PracticeA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("mypractice git");
+		System.out.println("change 2");
 	}
 
 }
